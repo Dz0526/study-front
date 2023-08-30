a9073751513dfa5d10706558997c08838e96328d
Dz0526 <this@dz99.me>
2023-08-30 17:44:45 +0900

    Feat: add Image component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,38 @@
 import type { NextPage } from 'next';
 import { Button } from 'components/Elements/Button';
+import { Image } from 'components/Elements/Image';
 
 const Home: NextPage = () => {
   return (
-    <Button variant='danger' size='lg'>
-      aiueo
-    </Button>
+    <>
+      <Button variant='danger' size='lg'>
+        aiueo
+      </Button>
+      <Image
+        src='https://i.postimg.cc/Hk2SXvJF/nord-pop.png'
+        ratio={1 / 1}
+        alt='1/1'
+        width={'240px'}
+      />
+      <Image
+        src='https://i.postimg.cc/Hk2SXvJF/nord-pop.png'
+        ratio={16 / 9}
+        alt='1/1'
+        width={'240px'}
+      />
+      <Image
+        src='https://i.postimg.cc/Hk2SXvJF/nord-pop.png'
+        ratio={4 / 3}
+        alt='1/1'
+        width={'240px'}
+      />
+      <Image
+        src='https://i.postimg.cc/Hk2SXvJF/nord-pop.png'
+        ratio={3 / 2}
+        alt='1/1'
+        width={'240px'}
+      />
+    </>
   );
 };
 
