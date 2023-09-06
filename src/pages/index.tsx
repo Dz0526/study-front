7919db9d4efbcebd9424edca526faf028a1c622b
Dz0526 <this@dz99.me>
2023-09-06 15:59:55 +0900

    Fix: index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,68 +1,29 @@
-import type { NextPage } from 'next';
-import { Button } from 'components/Elements/Button';
-import { Image } from 'components/Elements/Image';
-import {
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-} from 'components/Elements/Menu';
+import { Box, Flex } from '@chakra-ui/react';
 import { MainLayout } from 'components/Layout/MainLayout';
 import { UserProfilePreview } from 'features/user/components/UserProfilePreview';
+import { WorksList } from 'features/works/components/WorksList';
 
-const Home: NextPage = () => {
+const MyPage = () => {
   return (
     <MainLayout>
-      <UserProfilePreview
-        name='Taichi Okuda'
-        description='つれづれなるまゝに、日暮らし、硯にむかひて、心にうつりゆくよしなし事を、そこはかとなく書きつくれば、あやしうこそものぐるほしけれ。（Wikipediaより）'
-        link={{
-          twitter: 'https://twitter.com',
-          facebook: 'https://facebook.com',
-          instagram: '',
-          youtube: '',
-        }}
-      />
-      <Button variant='danger' size='lg'>
-        aiueo
-      </Button>
-      <Image
-        src='https://i.postimg.cc/Hk2SXvJF/nord-pop.png'
-        ratio={1 / 1}
-        alt='1/1'
-        width={'240px'}
-      />
-      <Image
-        src='https://i.postimg.cc/Hk2SXvJF/nord-pop.png'
-        ratio={16 / 9}
-        alt='1/1'
-        width={'240px'}
-      />
-      <Image
-        src='https://i.postimg.cc/Hk2SXvJF/nord-pop.png'
-        ratio={4 / 3}
-        alt='1/1'
-        width={'240px'}
-      />
-      <Image
-        src='https://i.postimg.cc/Hk2SXvJF/nord-pop.png'
-        ratio={3 / 2}
-        alt='1/1'
-        width={'240px'}
-      />
-      <Menu>
-        <MenuButton>aiueo</MenuButton>
-        <MenuList>
-          <MenuItem>kami</MenuItem>
-          <MenuDivider />
-          <MenuItem>kami</MenuItem>
-          <MenuDivider />
-          <MenuItem>kami</MenuItem>
-        </MenuList>
-      </Menu>
+      <Flex py={'lg'} px={'xl'} gap={'lg'}>
+        <Box flexBasis={'20%'}>
+          <UserProfilePreview
+            name='Taichi Okuda'
+            description='つれづれなるまゝに、日暮らし、硯にむかひて、心にうつりゆくよしなし事を、そこはかとなく書きつくれば、あやしうこそものぐるほしけれ。（Wikipediaより）'
+            link={{
+              twitter: 'https://twitter.com',
+              facebook: 'https://facebook.com',
+              instagram: '',
+              youtube: '',
+            }}
+          />
+        </Box>
+        <Box flexBasis={'80%'}>
+          <WorksList />
+        </Box>
+      </Flex>
     </MainLayout>
   );
 };
-
-export default Home;
+export default MyPage;
